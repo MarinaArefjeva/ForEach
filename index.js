3c7b48ab08cb25dd781fd3e72ba43d79b19fd27c
MarinaArefjeva <130138294+MarinaArefjeva@users.noreply.github.com>
2023-09-08 13:34:52 +0200

    ForEach

---
+++ index.js
@@ -0,0 +1,69 @@
+//1
+// let arr = [4, 5, 6, 10, 1];
+
+// array.forEach((el) => console.log(el));
+
+//2
+// let arr = [4, 5, 6, 10, 1];
+
+// arr.forEach((el=> {
+//   result = el * 7;
+//   console.log(result);
+// });
+//[28, 35, 42, 70, 7]
+
+//3
+
+// let arr = [-7, 9, -10, 6, -3];
+
+// arr.forEach((el) => {
+//   if (el > 0) {
+//     console.log(el);
+//   }
+// });
+
+//4
+
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.forEach((el, ind, arr) => {
+//   arr[ind] = el * 2;
+// });
+// console.log(arr); //[2, 4, 6, 8, 10]
+
+//5
+
+// let arr = [1, 5, 3, 8, 2];
+
+// let maxValue = arr[0];
+
+// arr.forEach((el) => {
+//   if (el > maxValue) {
+//     maxValue = el;
+//   }
+// });
+// console.log(maxValue);
+
+// let minValue = arr[0];
+
+// arr.forEach((el) => {
+//   if (el > minValue) {
+//     maxValue = el;
+//   }
+// });
+// console.log(minValue);
+
+//6
+// const originalArr = [
+//   { name: "Alice", age: 30 },
+//   { name: "Bob", age: 25 },
+//   { name: "Charlie", age: 35 },
+// ];
+
+// const newArr = [];
+
+// originalArr.forEach((item) => {
+//   newArr.push(item.name);
+// });
+
+// console.log(newArr);
